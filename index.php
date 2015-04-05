523fe8c475467419cfb64f627e17838f1d170ef6
Tony Lefler <tonylefler@gmail.com>
2015-04-05 13:48:26 -0600

    header is responsive

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+        The Five 12 Foundation
+    </title>
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="lib/main.css">
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <img class="logo" src="img/logo.png" alt="">
+        <div class="nav pull-left">
+            <button class="menu-btn">
+                Purpose
+            </button>
+            <button class="menu-btn">
+                Process
+            </button>
+            <button class="menu-btn">
+                Stats
+            </button>
+            <button class="menu-btn">
+                Take Action
+            </button>
+            <button class="menu-btn">
+                About
+            </button>
+        </div>
+        <div class="nav pull-right">
+            <button class="menu-btn menu-btn-donate">
+                Donate
+            </button>
+            <button class="menu-btn menu-btn-hamburger">
+                <i class="glyphicon glyphicon-menu-hamburger"></i>
+            </button>
+        </div>
+    </div>
+</header>
+
+
+
+<div id="hero-unit" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#hero-unit" data-slide-to="0" class="active"></li>
+        <li data-target="#hero-unit" data-slide-to="1"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item item-slide-1 active">
+            <div class="carousel-caption">
+                hey hey hey
+            </div>
+        </div>
+        <div class="item item-slide-2">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#hero-unit" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#hero-unit" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<section class="section">
+    <div class="container">
+        <div class="lead text-center">
+            Purpose Statement of weekend backpacks: one sentence.
+        </div>
+    </div>
+</section>
+
+<script src="lib/jquery/jquery.min.js"></script>
+<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="lib/main.js"></script>
+</body>
+</html>
